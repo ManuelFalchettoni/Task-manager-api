86c95f2f98fc5d4ac804b8872eca2ac0f11afad4
ManuelFalchettoni <manufalchettoni@gmail.com>
2026-04-26 20:33:01 -0300

    fix: add validation annotations to TaskRequest

--- src/main/java/com/ManuelFalchettoni/task_manager_api/dto/request/TaskRequest.java
+++ src/main/java/com/ManuelFalchettoni/task_manager_api/dto/request/TaskRequest.java
@@ -1,8 +1,12 @@
 package com.ManuelFalchettoni.task_manager_api.dto.request;
 
 import com.ManuelFalchettoni.task_manager_api.enums.TaskStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public class TaskRequest {
+    @NotBlank
+    @Size(max = 50)
     private String title;
     private String description;
     private TaskStatus state;
