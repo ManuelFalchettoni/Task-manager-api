2df994e0f6917d07a0255cfcbd83828a5333dc68
ManuelFalchettoni <manufalchettoni@gmail.com>
2026-04-30 12:45:27 -0300

    test: add unit test for TaskService.find happy path and sad path

--- src/main/java/com/ManuelFalchettoni/task_manager_api/exception/task/TaskNotFoundException.java
+++ src/main/java/com/ManuelFalchettoni/task_manager_api/exception/task/TaskNotFoundException.java
@@ -3,7 +3,7 @@ package com.ManuelFalchettoni.task_manager_api.exception.task;
 
 public class TaskNotFoundException extends RuntimeException {
     public TaskNotFoundException(Long id) {
-        super("Task not found "+ id);
+        super("Task not found with id: "+ id);
     }
 
 }
--- src/test/java/com/ManuelFalchettoni/task_manager_api/TaskServiceTest.java
+++ src/test/java/com/ManuelFalchettoni/task_manager_api/TaskServiceTest.java
@@ -4,6 +4,7 @@ import com.ManuelFalchettoni.task_manager_api.dto.request.task.TaskCreateRequest
 import com.ManuelFalchettoni.task_manager_api.dto.response.TaskResponse;
 import com.ManuelFalchettoni.task_manager_api.entity.task.Task;
 import com.ManuelFalchettoni.task_manager_api.enums.TaskStatus;
+import com.ManuelFalchettoni.task_manager_api.exception.task.TaskNotFoundException;
 import com.ManuelFalchettoni.task_manager_api.mapper.TaskMapper;
 import com.ManuelFalchettoni.task_manager_api.repository.task.JpaTaskRepository;
 import com.ManuelFalchettoni.task_manager_api.service.task.TaskService;
@@ -13,6 +14,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Locale;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -32,7 +36,7 @@ public class TaskServiceTest {
                 "Titulo",
                 "Description"
         );
-        // Configuramos el comportamiento: when -> thenReturn
+        // when -> thenReturn
         Task task = TaskMapper.requestToTask(request);
         ReflectionTestUtils.setField(task, "id", 1L);
         when(jpaTaskRepository.save(any(Task.class))).thenReturn(task);
@@ -40,12 +44,47 @@ public class TaskServiceTest {
         // WHEN (Acción)
         TaskResponse response = taskService.create(request);
 
-        // THEN (Verificación)
+        // THEN
         assertNotNull(response);
         assertEquals(1L, response.getId());
         assertEquals("Titulo", response.getTitle());
         assertEquals("Description", response.getDescription());
         assertEquals(TaskStatus.PENDING, response.getState());
+
+    }
+
+    @Test
+    void find_ShouldReturnTask_WhenValidID() {
+        Task task = new Task(
+                "Titulo",
+                "Description"
+        );
+        ReflectionTestUtils.setField(task, "id", 1L);
+        when(jpaTaskRepository.findById(1L)).thenReturn(Optional.of(task));
+
+        //When
+        TaskResponse response = taskService.find(1L);
+
+        //Then
+        assertNotNull(response);
+        assertEquals(1L, response.getId());
+        assertEquals("Titulo", response.getTitle());
+        assertEquals("Description", response.getDescription());
+        assertEquals(TaskStatus.PENDING, response.getState());
+    }
+
+    @Test
+    void find_ShouldThrowException_WhenInvalidID(){
+        // GIVEN: Setup the mock to return an empty box (Optional.empty)
+        when(jpaTaskRepository.findById(1L)).thenReturn(Optional.empty()); //
+
+        // WHEN & THEN: Assert that the exception is thrown
+        TaskNotFoundException exception = assertThrows(TaskNotFoundException.class, () ->{
+            taskService.find(1L);
+        });
+
+        assertEquals("Task not found with id: " + 1L, exception.getMessage());
+
 
     }
 }
