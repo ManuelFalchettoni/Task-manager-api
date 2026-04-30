4b503fa8b9eefe5c6771db02afcb5d676da449a8
ManuelFalchettoni <manufalchettoni@gmail.com>
2026-04-30 13:06:48 -0300

    test: add unit test for TaskService.update happy path and sad path

--- src/test/java/com/ManuelFalchettoni/task_manager_api/TaskServiceTest.java
+++ src/test/java/com/ManuelFalchettoni/task_manager_api/TaskServiceTest.java
@@ -1,6 +1,7 @@
 package com.ManuelFalchettoni.task_manager_api;
 
 import com.ManuelFalchettoni.task_manager_api.dto.request.task.TaskCreateRequest;
+import com.ManuelFalchettoni.task_manager_api.dto.request.task.TaskUpdateRequest;
 import com.ManuelFalchettoni.task_manager_api.dto.response.TaskResponse;
 import com.ManuelFalchettoni.task_manager_api.entity.task.Task;
 import com.ManuelFalchettoni.task_manager_api.enums.TaskStatus;
@@ -50,7 +51,6 @@ public class TaskServiceTest {
         assertEquals("Titulo", response.getTitle());
         assertEquals("Description", response.getDescription());
         assertEquals(TaskStatus.PENDING, response.getState());
-
     }
 
     @Test
@@ -75,7 +75,7 @@ public class TaskServiceTest {
 
     @Test
     void find_ShouldThrowException_WhenInvalidID(){
-        // GIVEN: Setup the mock to return an empty box (Optional.empty)
+        // GIVEN: Set up the mock to return an empty box (Optional.empty)
         when(jpaTaskRepository.findById(1L)).thenReturn(Optional.empty()); //
 
         // WHEN & THEN: Assert that the exception is thrown
@@ -84,7 +84,40 @@ public class TaskServiceTest {
         });
 
         assertEquals("Task not found with id: " + 1L, exception.getMessage());
+    }
 
+    @Test
+    void update_ShouldReturnTask_WhenValidRequest(){
+        // GIVEN
+        TaskUpdateRequest request = new TaskUpdateRequest("Title", "Description 2", TaskStatus.IN_PROGRESS);
+
+        Task task = new Task("Titulo", "Description");
+        ReflectionTestUtils.setField(task, "id", 1L);
+
+
+        when(jpaTaskRepository.findById(1L)).thenReturn(Optional.of(task));
+        when(jpaTaskRepository.save(any(Task.class))).thenReturn(task);
+
+        TaskResponse response = taskService.update(1L, request);
+
+        assertNotNull(response);
+        assertEquals(1L, response.getId());
+        assertEquals("Title", response.getTitle());
+        assertEquals("Description 2", response.getDescription());
+    }
+
+    @Test
+    void update_ShouldThrowException_WhenTaskNotFound(){
+        // GIVEN
+        TaskUpdateRequest request = new TaskUpdateRequest("Title", "Description 2", TaskStatus.IN_PROGRESS);
+        when(jpaTaskRepository.findById(1L)).thenReturn(Optional.empty());
+
+        // WHEN & THEN: Assert that the exception is thrown
+        TaskNotFoundException exception = assertThrows(TaskNotFoundException.class, () ->{
+            taskService.update(1L,request);
+        });
+
+        assertEquals("Task not found with id: " + 1L, exception.getMessage());
 
     }
 }
